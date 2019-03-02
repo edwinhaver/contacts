63b19bc08c67e3787b8c9e4743b2c5b1732b9e35
Edwin Haver <edwinhaver@gmail.com>
2019-03-02 19:46:47 +0400

    Setup mobile navigation

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Accounts} from 'meteor/accounts-base';
 import PropTypes from 'prop-types';
 import { createContainer, withTracker } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 // export default class PrivateHeader extends React.Component {
 //   onLogout() {
@@ -18,9 +19,11 @@ import { createContainer, withTracker } from 'meteor/react-meteor-data';
 // }
 
 export const PrivateHeader = (props) => {
+  const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
   return (
     <div className="header">
       <div className="header__content">
+        <img className="header__nav-toggle" src={navImageSrc} onClick={props.handleNavToggle}/>
         <h1 className="header__title">{props.title}</h1>
         <button className="button button--link-text" onClick={() => props.handleLogout()}>Log out</button>
       </div>
@@ -30,11 +33,15 @@ export const PrivateHeader = (props) => {
 
 PrivateHeader.propTypes = {
   title: PropTypes.string.isRequired,
-  handleLogout: PropTypes.func.isRequired
+  handleLogout: PropTypes.func.isRequired,
+  handleNavToggle: PropTypes.func.isRequired,
+  isNavOpen: PropTypes.bool.isRequired
 };
 
 export default createContainer(() => {
   return {
-    handleLogout: () => Accounts.logout()
+    handleLogout: () => Accounts.logout(),
+    handleNavToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+    isNavOpen: Session.get('isNavOpen')
   };
 }, PrivateHeader);
